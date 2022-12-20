404821dd0ee7ba1c7635ba1518bc522f9ab5597c
GuglielmoBini <guglielmobini@gmail.com>
2022-12-20 19:21:00 +0100

    creata funzione countdown e stampo in pagina

--- script.js
+++ script.js
@@ -21,3 +21,42 @@ const countSeconds = document.getElementById("seconds");
 
 // setto la data del countdown
 const countDownDate = new Date("Dec 25, 2022 00:00:00").getTime();
+
+//converto da ms in secondi, minuti, ore e giorni
+const seconds = 1000;
+const minutes = seconds * 60;
+const hours = minutes * 60;
+const days = hours * 24;
+
+// creo funzione per il countdown (1 sec)
+
+const countDown = setInterval(() => {
+  // prendo la data di oggi
+  const today = new Date().getTime();
+  //   console.log(today);
+
+  // trovo la differenze tra la data di oggi e quella della fine del mio countdown
+  const difference = countDownDate - today;
+
+  // calcolo il tempo per giorni, ore, minuti e secondi
+  const daysLeft = Math.floor(difference / days);
+  //   console.log("giorni:" + " " + daysLeft);
+  const hoursLeft = Math.floor((difference % days) / hours);
+  //   console.log("ore:" + " " + hoursLeft);
+  const minutesLeft = Math.floor((difference % hours) / minutes);
+  //   console.log("minuti:" + " " + minutesLeft);
+  const secondsLeft = Math.floor((difference % minutes) / seconds);
+  //   console.log("secondi:" + " " + secondsLeft);
+
+  // stampo in pagina
+  countDays.innerText = daysLeft;
+  countHours.innerText = hoursLeft;
+  countMinutes.innerText = minutesLeft;
+  countSeconds.innerText = secondsLeft;
+
+  // stoppo il countdown
+
+  if (difference < 0) {
+    clearInterval(countDown);
+  }
+}, 1000);
